e9bd45d59cd821d9693c8a05de7bfb87ba8bffd6
mahmoud khaled <54672453+mahmoudkahled@users.noreply.github.com>
2023-04-23 13:40:18 +0200

    finish implementing trinin_best_model.py which train the final best model and prints some metrics

---
+++ src/train_best_model.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import os
+from models import *
+from model_selection import *
+from sklearn.metrics import confusion_matrix, classification_report
+
+def choose_best_model(report):
+    best_model = report.idxmax(axis=1)
+    best_model = best_model[0]
+    return best_model
+
+def confusion_mat(yTest, y_pred):
+    cm = confusion_matrix(yTest, y_pred) ## confusion matrix
+    cr = classification_report(yTest,y_pred) ## classification report
+    return cm, cr
+
+def print_info(best_model, best_params, score, cm, cr):
+    print('The best model is: ', best_model)
+    print('The best model parameters are: ', best_params)
+    print('The best model score is: ', score)
+    print('The confusion matrix is: \n', cm)
+    print('The classification report is: \n', cr)
+
+def get_train_data():
+    curr_dir = os.getcwd()
+    train_path = 'C:/Users/mahmo/Desktop/COVID-19-Outcome-Prediction/outputs/train.csv'
+    test_path = 'C:/Users/mahmo/Desktop/COVID-19-Outcome-Prediction/outputs/test.csv'
+    train = pd.read_csv(os.path.join(curr_dir, train_path))
+    test = pd.read_csv(os.path.join(curr_dir, test_path))
+    X_train, yTrain = split_data(train)
+    X_test, yTest = split_data(test)
+    report_path = 'C:/Users/mahmo/Desktop/COVID-19-Outcome-Prediction/outputs/models_score.csv'
+    report = pd.read_csv(os.path.join(curr_dir, report_path))
+    return X_train, yTrain, X_test, yTest, report
+
+if __name__ == '__main__':
+    X_train, yTrain, X_test, yTest, report = get_train_data()
+    best_model = choose_best_model(report)
+    model = create_model(best_model)
+    best_params = optimize_model(X_train, yTrain, model, best_model)
+    y_pred = train_optimized_model(X_train, yTrain, X_test, best_params, best_model, "class")
+    cm, cr = confusion_mat(yTest, y_pred)
+    score = roc_score(yTest, X_test, y_pred)
+    print_info(best_model, best_params, score, cm, cr)
