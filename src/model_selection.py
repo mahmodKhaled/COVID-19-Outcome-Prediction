2ca02e1c6e2896e3320ec20a5e9e3e34743e0ef3
mahmoud khaled <54672453+mahmoudkahled@users.noreply.github.com>
2023-04-23 19:06:48 +0200

    update the model_selection.py paths and correct them

--- src/model_selection.py
+++ src/model_selection.py
@@ -2,6 +2,8 @@ from sklearn.metrics import roc_auc_score
 import pandas as pd
 import os
 from process_data import *
+from models import *
+
 def roc_score(yTest, X_test, preds):
     score = roc_auc_score(yTest, preds)
     return score
@@ -18,14 +20,19 @@ def models_report(models_names, models_score):
     for i in range(len(models_names)):
         models_score_dic[models_names[i]] = models_score[i]
     models_score_df = pd.DataFrame(models_score_dic, columns=models_names, index= [0])
-    models_score_df.to_csv(os.path.join(curr_dir, 'outputs/models_score.csv'), index=False)
+    curr_dir = os.getcwd()
+    output_path = 'outputs'
+    if not os.path.exists(os.path.join(curr_dir, output_path)):
+        os.makedirs(os.path.join(curr_dir, output_path))
+    output_path = 'outputs'
+    models_score_path = output_path + '/models_score.csv'
+    models_score_df.to_csv(os.path.join(curr_dir, models_score_path), index=False)
 
 if __name__ == "__main__":
     curr_dir = os.getcwd()
-    test_path = 'C:/Users/mahmo/Desktop/COVID-19-Outcome-Prediction/outputs/test.csv'
-    test = pd.read_csv(os.path.join(curr_dir, test_path))
-    X_test, yTest = split_data(test)
-    models_preds_path = 'C:/Users/mahmo/Desktop/COVID-19-Outcome-Prediction/outputs/models_preds.csv'
+    test_path = 'outputs/test.csv'
+    X_test, yTest = read_and_split_data(test_path)
+    models_preds_path = 'outputs/models_preds.csv'
     models_preds = pd.read_csv(os.path.join(curr_dir, models_preds_path))
     models_names, models_score = evaluate_models(X_test, yTest, models_preds)
     models_report(models_names, models_score)
